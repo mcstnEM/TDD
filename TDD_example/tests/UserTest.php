b5051ce611cc80aeaf4f93b028e30cdf4df624ea
mathieuCstn <mathieuconstantin01@gmail.com>
2024-02-26 12:07:20 +0100

    Calculator.php file hase been added vendor/autoload.php hase been added composer.json file hase been updated phpunit.xml configuration file hase been updated

--- TDD_example/tests/UserTest.php
+++ TDD_example/tests/UserTest.php
@@ -4,15 +4,25 @@ use PHPUnit\Framework\Attributes\TestDox;
 use App\User;
 
 class UserTest extends TestCase {
+    protected User $user;
+
+    public function setUp(): void 
+    {
+        $this->user = new User('Jeremi', 'Duffay');
+    }
 
     #[TestDox('test the hello function')]
     public function testHello(): void
     {
-        $user = new User('Jeremi', 'Duffay');
 
-        $result = $user->hello('François');
+        $result = $this->user->hello('François');
 
         # On test si $reslut est bien le resultat attendu
         $this->assertSame('Hello François, I am Jeremi', $result);
+    }
+
+    public function testGetName() {
+        $result = $this->user->getName();
+        $this->assertSame('Jeremi', $result);
     }
 }
